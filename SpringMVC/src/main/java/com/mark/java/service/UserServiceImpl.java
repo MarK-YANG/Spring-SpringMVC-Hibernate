5a6e42ad7b84a8b1fad3a1517954d4f9bc0c8dff
MarK <emp.yangchunyu@gmail.com>
2015-05-09 11:38:18 +0800

    import org.springframework.transaction.annotation.Transactional;

--- SpringMVC/src/main/java/com/mark/java/service/UserServiceImpl.java
+++ SpringMVC/src/main/java/com/mark/java/service/UserServiceImpl.java
@@ -5,7 +5,7 @@ import com.mark.java.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 /**
